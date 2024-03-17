43fdac74162e6fe73876aa89df18ce8e66bc7d52
Marinee Nicole <147280991+marineenicole@users.noreply.github.com>
2024-03-17 09:37:09 -0700

    Fixed the table name from categories to Category and added values.

--- DATABASE/inserts.sql
+++ DATABASE/inserts.sql
@@ -1,20 +1,17 @@
 -- File for inserting data
 
--- Insert the category type && section: , Meats, or Dairy.
-INSERT INTO categories(productType, productSection) VALUES (
+-- Insert the category type && section
+INSERT INTO Category(productType, productSection) VALUES
     
     ('Vegetable', 'Produce'),
     ('Fruit', 'Produce'),
-    
     ('Meat', 'Meats'),
     ('Poultry', 'Meats'),
     ('Seafood', 'Meats'),
-
     ('Milk', 'Dairy'),
     ('Yogurt', 'Dairy'),
     ('Cheese', 'Dairy'),
     ('Cream', 'Dairy'),
     ('Eggs', 'Dairy'),
     ('Butter', 'Dairy'),
-    ('Plant-Based', 'Dairy')
-);
+    ('Plant-Based', 'Dairy');
