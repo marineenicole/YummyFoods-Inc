d2102761d7cd4c982ccd1f7ec63ea1ac7439dbaf
Marinee Nicole <147280991+marineenicole@users.noreply.github.com>
2024-03-17 09:22:29 -0700

    Added values to the category table

--- DATABASE/inserts.sql
+++ DATABASE/inserts.sql
@@ -1,7 +1,20 @@
 -- File for inserting data
 
--- Insert the category type for categories table: Veggetables, Meats, or Dairy.
-INSERT INTO categories(productSection) VALUES (
+-- Insert the category type && section: , Meats, or Dairy.
+INSERT INTO categories(productType, productSection) VALUES (
     
+    ('Vegetable', 'Produce'),
+    ('Fruit', 'Produce'),
+    
+    ('Meat', 'Meats'),
+    ('Poultry', 'Meats'),
+    ('Seafood', 'Meats'),
 
+    ('Milk', 'Dairy'),
+    ('Yogurt', 'Dairy'),
+    ('Cheese', 'Dairy'),
+    ('Cream', 'Dairy'),
+    ('Eggs', 'Dairy'),
+    ('Butter', 'Dairy'),
+    ('Plant-Based', 'Dairy')
 );
