06561b057235032ee71bd4f44cc7852197a9cf4c
Marinee Nicole <147280991+marineenicole@users.noreply.github.com>
2024-03-20 14:13:23 -0700

    Created a testing file for checking that data was implemented correctly.

---
+++ DATABASE/test/testing.sql
@@ -0,0 +1,21 @@
+-- TESTING FILE 
+/*
+    This file is for checking if the database was implemented correctly.
+*/
+
+-- After creating the database
+SHOW DATABASES;
+
+USE YummyFoods;
+
+-- Check if tables were created
+SHOW TABLES;
+
+-- Describes the contents of specified table
+DESCRIBE Category;
+DESCRIBE Products;
+DESCRIBE Contact;
+DESCRIBE Employees;
+DESCRIBE Transactions;
+DESCRIBE Suppliers;
+
